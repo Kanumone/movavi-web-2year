fc9b1729b968f26e82af3fb4238d7d94e7d1e5bd
Kanumone <KuzminAAwork@yandex.ru>
2023-12-09 20:25:25 +0700

    hbs

--- projects/rock-paper-scissors/backend/app.js
+++ projects/rock-paper-scissors/backend/app.js
@@ -1,12 +1,25 @@
-const express = require('express')
+const express = require('express');
+const handlebars = require('express-handlebars');
+const frontend = __dirname + '/../frontend';
 
 const app = express();
 
-app.get('/hello', function (req, res) {
-    res.end('hello, world!');
+app.engine(
+    '.hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        defaultLayout: 'main',
+    })
+);
+app.set('views', `${frontend}/views`);
+app.set('view engine', '.hbs');
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Greetings form Handlebars',
+        qwe: 123
+    });
 });
 
-app.use('/', express.static(`${__dirname}/../frontend`));
 app.listen(5000, function () {
     console.log('Server started!');
 });
